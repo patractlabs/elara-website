d0582cba19446b9ef346c972c4fa7de211996aca
zhb666 <462612421@qq.com>
2020-11-19 12:04:05 +0800

    fix: 登陆bug

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { useTranslation } from 'react-i18next'
+import { createHashHistory } from "history"; // 如果是history路由
+import { Prompt } from 'react-router-dom'
+
 import { login } from "../../Api/Interface";
 import userCounterModel from "../Hox/User";
-import { useTranslation} from 'react-i18next'
-import { createHashHistory } from "history"; // 如果是history路由
-import {URL_ACCOUNT} from '../../Config/origin'
 
-
+import { URL_ACCOUNT } from '../../Config/origin'
 import PageH from "../../utils/pageHeight";
 
 import Footer from "../Footer/index";
@@ -15,31 +16,41 @@ import "./index.css";
 const imgList = [require("../assets/Github.svg")];
 let off = true
 
-interface prposChild {
-  openWindow: Function;
-}
+// interface prposChild {
+//   openWindow: Function;
+//   Prompt:any
+// }
 
-const Login: React.FC<prposChild> = ({ openWindow }) => {
+
+
+// const Login: React.FC<prposChild> = ({ openWindow, Prompt}) => {
+const Login = () => {
   const history = createHashHistory();
   const userInfo = userCounterModel();
   const { t } = useTranslation();
-  // useEffect(() => {
-  //   loginInit();
-  // }, []);
+  useEffect(() => {
+    loginInit();
+  }, []);
 
-  openWindow = () => {
+  const confirmToSave = (location) => {
+    console.log('准备离开了吗')
+    off = true
+    return false
+  }
+
+  const openWindow = () => {
     window.open(URL_ACCOUNT + "/auth/github");
 
-    window.onmessage=function(ev: { data: any; }) {
-      if(off){
+    window.onmessage = function (ev) {
+      if (off) {
         const data = ev.data;
         console.log(ev.data, "进来了吗");
         localStorage.setItem("token", "123456");
         loginInit();
         off = false
       }
-      
-     }
+
+    }
 
     //接受登陆传回来的值
     // window.addEventListener(
@@ -77,6 +88,12 @@ const Login: React.FC<prposChild> = ({ openWindow }) => {
 
   return (
     <div className="login animated fadeInLeft">
+      <Prompt
+        when={true}
+        message={(location) => {
+          confirmToSave(location)
+        }}
+      />
       <div className="loginMain" style={{ height: PageH(128) }}>
         <h2>{t("sign.Sign")}</h2>
         <div
