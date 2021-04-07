f69b2ad903bac886fdc373063e14fcb29d0f3d5b
freepoix <17864213831@163.com>
2021-04-07 14:58:38 +0800

    io => cn

--- src/config/origin.ts
+++ src/config/origin.ts
@@ -7,21 +7,21 @@ if (process.env.REACT_APP_NODE_ENV === 'development') {
   LOGIN_DOMAIN = '/accountApi';
   API_DOMAIN = '/accountApi';
 
-  ENDPOINTS_URL = "https://test-api.elara.patract.io";
-  WSS_ENDPOINTS_URL = "wss://test-api.elara.patract.io";
+  ENDPOINTS_URL = "https://test-api.elara.patract.cn";
+  WSS_ENDPOINTS_URL = "wss://test-api.elara.patract.cn";
 } else if (process.env.REACT_APP_NODE_ENV === 'pre-production') {
-  LOGIN_DOMAIN = 'https://test-elara.patract.io';
+  LOGIN_DOMAIN = 'https://test-elara.patract.cn';
   // LOGIN_DOMAIN = 'https://elara-website-git-xyl-dev-patract.vercel.app';
-  API_DOMAIN = 'https://test-elara.patract.io';
+  API_DOMAIN = 'https://test-elara.patract.cn';
 
-  ENDPOINTS_URL = "https://test-api.elara.patract.io";
-  WSS_ENDPOINTS_URL = "wss://test-api.elara.patract.io";
+  ENDPOINTS_URL = "https://test-api.elara.patract.cn";
+  WSS_ENDPOINTS_URL = "wss://test-api.elara.patract.cn";
 } else if (process.env.REACT_APP_NODE_ENV === 'production') {
-  LOGIN_DOMAIN = 'https://elara.patract.io';
-  API_DOMAIN = 'https://elara.patract.io';
+  LOGIN_DOMAIN = 'https://elara.patract.cn';
+  API_DOMAIN = 'https://elara.patract.cn';
 
-  ENDPOINTS_URL = "https://api.elara.patract.io";
-  WSS_ENDPOINTS_URL = "wss://api.elara.patract.io";
+  ENDPOINTS_URL = "https://api.elara.patract.cn";
+  WSS_ENDPOINTS_URL = "wss://api.elara.patract.cn";
 }
 
 export {
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,21 +1,10 @@
 const proxy = require('http-proxy-middleware')
  
 module.exports = function(app) {
-  // app.use(
-  //   '/statApi',
-  //   proxy.createProxyMiddleware({
-  //     target: 'https://test-elara.patract.io',
-  //     changeOrigin: true,
-  //     pathRewrite: {
-  //       '^/statApi': ''
-  //     }
-  //   })
-  // );
-
   app.use(
     '/accountApi',
     proxy.createProxyMiddleware({
-      target: 'https://test-elara.patract.io',
+      target: 'https://test-elara.patract.cn',
       changeOrigin: true,
       pathRewrite: {
         '^/accountApi': ''
