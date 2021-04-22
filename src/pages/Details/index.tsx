ea54fa71f8a3f52a3586438a5217f62cf4e2b25d
freepoix <17864213831@163.com>
2021-04-22 14:48:34 +0800

    up

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -291,12 +291,12 @@ const Details: React.FC = () => {
             </div>
             
             <div className="endpoints">
-              <div className="active-row">ENDPOINTS (WSS)</div>
+              <div className="active-row">ENDPOINT (WSS)</div>
               <div className="info-row endpoints-row">
                 { `${WSS_ENDPOINTS_URL}/${project?.chain.toLowerCase()}/${project?.pid}` }
                 <img className="copy-img" onClick={ () => copy(`${WSS_ENDPOINTS_URL}/${project?.chain.toLowerCase()}/${project?.pid}`)} src={CopySVG} alt=""/>
               </div>
-              <div className="active-row">ENDPOINTS (HTTPS)</div>
+              <div className="active-row">ENDPOINT (HTTPS)</div>
               <div className="info-row endpoints-row">
                 
               <span>{ `${ENDPOINTS_URL}/${project?.chain.toLowerCase()}/${project?.pid}` }</span>
