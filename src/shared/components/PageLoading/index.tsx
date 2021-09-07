39e4f35b5b39ad728c41590e5f6bd9c98976b4c8
xsteadybcgo <xw4585@gmail.com>
2021-09-07 17:16:44 +0800

    feat: replace loading component

--- src/shared/components/PageLoading/index.tsx
+++ src/shared/components/PageLoading/index.tsx
@@ -1,10 +1,22 @@
-import { Spin } from 'antd'
+import lottie from 'lottie-web'
+import { useEffect, useRef } from 'react'
+import data from './data'
 import './index.css'
 
 export default function PageLoading() {
+  const ref = useRef(null!)
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: ref.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: data,
+    })
+  }, [])
   return (
     <div className="elara-loading-container">
-      <Spin size="large" spinning={true} className="elara-spin" />
+      <div className="elara-spin" ref={ref} />
     </div>
   )
 }
